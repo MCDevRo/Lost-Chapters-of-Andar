60ae3b7a4236bc1051ca66b7c418ad6e692ee704
ObsessiveCharr <marius.cristea@mcdev.ro>
2023-03-13 15:58:45 +0200

    QuickFix Update V1.0
    Short rotation tweak to fireball attack !

--- GameDevCamp/Assets/Scripts/Player/PlayerAttackController.cs
+++ GameDevCamp/Assets/Scripts/Player/PlayerAttackController.cs
@@ -9,6 +9,7 @@ public class PlayerAttackController : MonoBehaviour
     [SerializeField] private float fireballSpeed = 20f;
     [SerializeField] private float fireballDuration = 5f;
     [SerializeField] private float fireballRange = 50f;
+    private TopDownPlayerController player;
 
     private bool isAttacking = false;
     private bool isFiring = false;
@@ -64,6 +65,7 @@ public class PlayerAttackController : MonoBehaviour
     {
         // Instantiate the fireball prefab at the spawn point
         GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPoint.position, Quaternion.identity);
+        player = GetComponent<TopDownPlayerController>();
 
         // set the fireball's velocity to the direction of the mouse pointer
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -71,6 +73,14 @@ public class PlayerAttackController : MonoBehaviour
         {
             Vector3 direction = (hit.point - fireball.transform.position).normalized;
             fireball.GetComponent<Rigidbody>().velocity = direction * fireballSpeed;
+
+            // rotate the player to face the direction they're shooting
+            Vector3 lookDirection = new Vector3(direction.x, 0, direction.z);
+            if (lookDirection != Vector3.zero)
+            {
+                Quaternion rotation = Quaternion.LookRotation(lookDirection);
+                player.transform.rotation = rotation;
+            }
         }
 
         // Destroy the fireball after a set duration
